9e632ba1e324a924fd6889b0658e1b51c586e12c
Samuel Stevan <145078525+sam-arias@users.noreply.github.com>
2025-08-29 17:24:42 -0500

    Cambios en el diseño y boton guardar

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import {View,Text,Image,ScrollView,TextInput,Button,StyleSheet,Alert} from 'reac
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = [
-      nombre='',
-      apellido='',
-      email='',
-      telefono='',
-      descripcion='',
-      mostrarPerfil = false,
-      mensajeAprobacion='Perfil creado con éxito'
-    ];
+    this.state = {
+      nombre:'',
+      apellido:'',
+      email:'',
+      telefono:'',
+      descripcion:'',
+      mostrarPerfil: false,
+      mensajeAprobacion: 'Perfil creado con éxito'
+    };
   }
 
   // Método para cambiar el nombre
@@ -60,6 +60,40 @@ class App extends Component {
     });
   };
 
+  saveProfile = () => {
+    const {nombre, apellido, email, telefono, descripcion} = this.state;
+    // Validar que los campos obligatorios no esten vacios
+    // nombre, apellido y email
+    if (!nombre.trim() || !apellido.trim() || !email.trim()) {
+      Alert.alert('Campos Vacios',
+        'Los campos nombre, apellido y email no pueden estar vacios',
+        [{text: 'OK'}]
+      );
+      return;
+    };
+
+    // Validar el correo
+    if (!email.includes('@')) {
+      Alert.alert(
+        'Email invalido',
+        'Por favor ingresar un correo valido',
+        [{text: 'OK'}]
+      )
+      return;
+    };
+
+    this.setState({
+      mostrarPerfil: true
+    });
+    
+    Alert.alert(
+      'Perfil guardado',
+      'El perfil ha sido guardado con éxito',
+      [{text: 'OK'}]
+    )
+
+  };
+
   // Método para limpiar el formulario
   clearForm = () => {
     this.setState({
@@ -93,19 +127,19 @@ class App extends Component {
     return(
       <>
       <ScrollView style={styles.container}>
-        <View>
+        <View style={styles.profileContainer}>
           <Image
             source={{uri: 'https://cdn-icons-png.flaticon.com/512/6073/6073874.png'}}
             style={styles.imagen} // Se crea el estilo
             resizeMode='cover'
           />
           <Text style={styles.text}>Mi perfil de usuario</Text>
-          <Text style={styles.text}>Mi aplicación demo de react</Text>
+          <Text>Mi aplicación demo de react</Text>
         </View>
 
-        <View>
+        <View style={styles.formInfo}>
           <Text style={styles.header}>Nombre</Text>
-          <TextInput
+          <TextInput style={styles.textInfo}
             value={nombre}
             onChangeText={this.changeNameState}
             placeholder='Ingrese su nombre'
@@ -113,7 +147,7 @@ class App extends Component {
           />
 
           <Text style={styles.header}>Apellido</Text>
-          <TextInput
+          <TextInput style={styles.textInfo}
             value={apellido}
             onChangeText={this.changeLastNameState}
             placeholder='Ingrese su apellido'
@@ -121,7 +155,7 @@ class App extends Component {
           />
 
           <Text style={styles.header}>Email</Text>
-          <TextInput
+          <TextInput style={styles.textInfo}
             value={email}
             onChangeText={this.changeEmailState}
             placeholder='Ingrese su email'
@@ -129,7 +163,7 @@ class App extends Component {
           />
           
           <Text style={styles.header}>Télefono</Text>
-          <TextInput
+          <TextInput style={styles.textInfo}
             value={telefono}
             onChangeText={this.changeTelefonoState}
             placeholder='Ingrese su teléfono'
@@ -137,7 +171,7 @@ class App extends Component {
           />
 
           <Text style={styles.header}>Descripción</Text>
-          <TextInput
+          <TextInput style={styles.textInfo}
             value={descripcion}
             onChangeText={this.changeDescriptionState}
             placeholder='Ingrese su descripción'
@@ -146,17 +180,22 @@ class App extends Component {
 
         </View>
 
-        <View>
+        <View style={styles.button}>
 
-          <Button
+          <View>
+            <Button
             title='Guardar Perfil'
-            //onPress={} // Crear método para guardar
-          />
-
-          <Button style={styles.button}
+            onPress={this.saveProfile} // Crear método para guardar
+            />
+          </View>
+          
+          <View>
+            <Button
             title='Limpiar Formulario'
             onPress={this.clearForm}
-          />
+           />
+          </View>
+          
         </View>
 
       </ScrollView>
@@ -168,16 +207,16 @@ class App extends Component {
 }; // Cierre de la clase
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#F5F5F5'
   },
 
   header: {
-    backgroundColor: '#4059e6ff',
     alignItems: 'center',
-    marginBottom: 20,
-    padding: 30
+    padding: 15,
+    fontWeight: '600'
   },
 
   imagen: {
@@ -186,18 +225,44 @@ const styles = StyleSheet.create({
     borderRadius:30,
     marginBottom: 15,
     borderWidth: 4,
-    borderColor: 'white'
+    borderColor: 'white',
+    marginTop: 15
   },
 
   button: {
     color: '#FFF',
     fontSize: 16,
     fontWeight: '600',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    margin: 15
+  },
+
+  profileContainer: {
+    alignItems: 'center'
   },
 
   text: {
-    alignItems: 'center'
+    color: '#1b80fbff',
+    fontSize: 20
+  },
+
+  textInfo: {
+    borderRadius: 8,
+    backgroundColor: '#FFFFFF',
+    marginLeft: 10
+  },
+
+  formInfo: {
+    padding: 10,
+    margin:10,
+    backgroundColor: '#eee',
+    marginTop: 20,
+    borderRadius: 20,
+    paddingBottom: 30
   }
+
 })
 
 export default App;
